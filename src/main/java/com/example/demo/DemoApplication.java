42864684a38cae75d5129d34cb4437261b160b15
shree-77 <shreedhoni7@gmail.com>
2023-12-07 15:49:06 +0530

    Layers created

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.Student.Student;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
+
 
 @SpringBootApplication
 @RestController
@@ -18,17 +16,6 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping()
-	public List<Student> hello(){
-		return List.of(
-				new Student(
-						1L,
-						"Shree",
-						"shreeviswa7@gmail.com",
-						LocalDate.of(2002, Month.JUNE,5),
-						21
-				)
-		);
-	}
+
 
 }
