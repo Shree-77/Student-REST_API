0f223e82483c5657daff4aba9111c73a321f27d3
shree-77 <shreedhoni7@gmail.com>
2023-12-07 15:32:23 +0530

    Student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,16 @@ public class DemoApplication {
 	}
 
 	@GetMapping()
-	public List<String> hello(){
-		return List.of("Hello" , "Spring-Boot" , "World");
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Shree",
+						"shreeviswa7@gmail.com",
+						LocalDate.of(2002, Month.JUNE,5),
+						21
+				)
+		);
 	}
 
 }
